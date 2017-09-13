d128c00b9bc9690be759201b1265f5efce4d8c58
oubuy <1312398030@qq.com>
2017-09-13 19:28:46 +0800

    第二步内容渲染

--- src/js/course/add.js
+++ src/js/course/add.js
@@ -5,6 +5,6 @@ $('#course_Add').ajaxForm(function(data) {
     if (data.code == 200) {
         console.log(data.result.cs_id)
         alert('创建成功');
-        location.href = '/dist/html/course/edit1.html?cs_id=' + data.result.cs_id;
+        location.href = '/dist/html/course/edit1.html?cs_id=' + data.result.cs_id; //可以让每一页获取对应的Id号
     }
 })
--- src/js/course/edit2.js
+++ src/js/course/edit2.js
@@ -1,2 +1,12 @@
 require('../common/header');
 require('../common/aside');
+
+
+//请求获取id号
+var util = require('../common/util.js');
+var cs_id = util.getSearch('cs_id'); //因为是键值对形式，直接穿个键名，就可以获取对应的值
+//动态渲染数据展示信息
+$.get('/v6/course/picture', { cs_id: cs_id }, function(data) {
+    var html = template('edit2_two', data.result);
+    $('#edit2_step').append(html);
+})
