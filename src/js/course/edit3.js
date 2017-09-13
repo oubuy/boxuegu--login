bc6121e47b46040c39d571747a4500bb5821f359
oubuy <1312398030@qq.com>
2017-09-13 21:53:09 +0800

    模态框编辑

--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -11,4 +11,24 @@ $.get('/v6/course/lesson', { cs_id: cs_id }, function(data) {
         //追加内容
         $('#edit3_tlp').append(html);
     }
+});
+
+/**
+ + * 编辑章节_数据回显：
+ + * 1、因为章节列表是动态生成的，所以需要通过委托的方式给编辑按钮绑定click事件
+ + * 2、事件触发时获取按钮身上自定义属性记录的ct_id，用来请求接口获取数据
+ + * 3、数据渲染模态框模版，插入到页面中
+ + * */
+$(document).on('click', '.btn-lesson-edit', function() {
+    //获取当前的章节id tc_id
+    var data = {
+        ct_id: $(this).attr('data-id')
+    };
+    console.log(data)　
+    $.get('/v6/course/chapter/edit', data, function(data) {
+        if (data.code == 200) {
+            //获取当前id返还的对应的值，用来渲染
+            $('#chapterModal').html(template('edit3_modal', data.result))
+        }
+    })
 })
