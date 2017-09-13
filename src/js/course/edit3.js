dd1bbf0a42b48b85ac50dc4b3dfe6f655f78db73
oubuy <1312398030@qq.com>
2017-09-13 23:21:45 +0800

    课程模态框添加有bug

--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -20,24 +20,50 @@ $.get('/v6/course/lesson', { cs_id: cs_id }, function(data) {
  + * 3、数据渲染模态框模版，插入到页面中
  + * */
 $(document).on('click', '.btn-lesson-edit', function() {
-    //获取当前的章节id tc_id
+    //获取当前的章节id ct_id
     var data = {
         ct_id: $(this).attr('data-id')
     };
     // console.log(data)　
     $.get('/v6/course/chapter/edit', data, function(data) {
         if (data.code == 200) {
+            //自定义一个ct_id属性的值   后端需要这个值来区分修改的章节属于那个课程
+            data.result.ct_id = ct_id;
             //获取当前id返还的对应的值，用来渲染
             $('#chapterModal').html(template('edit3_modal', data.result))
         }
     })
 });
-//提交
+//提交模态框数据
 $('#edit3_Form').ajaxForm({
     delegation: true,
-    success: function(data) {
-        if (data.code == 200) {
-            alert('提交成功');
-        }
+    success: lessonSuccess
+});
+
+
+/**
+ + * 添加章节：
+ + * 1、因为表单要数据回显是动态生成的，所以使用委托的方式给添加按钮绑定click事件
+ + * 2、事件触发时使用一个空对象渲染模态框模版，插入到页面中
+ + * */
+$(document).on('click', '#btn-lesson-add', function() {
+    var html = template('edit3_modal', { cs_id: cs_id }); //cs_id必须要传
+    $('#chapterModal').html(html);
+});
+
+/**
+ + * 修改或添加章节成功后的处理回调：
+ + * 1、如果服务器返回的数据中有result那么证明是添加章节成功了，否则就是修改成功了
+ + * 2、不同的情况给出不同的用户提示，然后添加章节成功后清空表单
+ + * */
+
+
+function lessonSuccess(data) {
+
+    if (data.result) {
+        alert('添加成功');
+        $('#lesson-form').get(0).reset();
+    } else {
+        alert('修改成功');
     }
-})
+}
