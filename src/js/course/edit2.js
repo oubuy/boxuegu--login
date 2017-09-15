8f6f5d8316db21f373e944605a5429693f6206cc
oubuy <1312398030@qq.com>
2017-09-15 22:30:10 +0800

    上传图片

--- src/js/course/edit2.js
+++ src/js/course/edit2.js
@@ -13,5 +13,27 @@ $.get('/v6/course/picture', { cs_id: cs_id }, function(data) {
         data.result.editIndex = 2;
         var html = template('edit2_two', data.result);
         $('#edit2_step').append(html);
+        initPlugin();
     }
-})
+});
+
+function initPlugin() {
+    $('#uploadify').uploadify({
+        swf: '/lib/jquery-uploadify/uploadify.swf', // 这个是flash脚本，必须引入，不然无法选择文件
+        uploader: '/v6/uploader/cover', // 这个是上传图片的接口
+        fileTypeExts: '*.gif; *.jpg; *.png', // 这个用来限制上传图片的类型
+        fileObjName: 'cs_cover_original', // 这个用来设置提交给后端时，文件数据对应的name,stream 类型课程图片
+        formData: { // 如果接口需要额外的数据，通过这个配置来添加
+            cs_id: cs_id
+        },
+        buttonText: '上传',
+        buttonClass: 'btn btn-success btn-sm', //按钮的类  引入样式
+        onUploadSuccess: function(file, dataStr) {
+            //成功回调，动态渲染图片，dataStr是返还的结果，不过是字符串
+            var data = JSON.parse(dataStr);
+            $('.preview img').attr('src', data.result.path);
+
+        }
+
+    })
+}
